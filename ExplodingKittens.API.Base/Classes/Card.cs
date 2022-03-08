e0b7cf946c25588fd9429e79d6935b28d6dd8108
Алексей Нестеров <nester.a.l@yandex.ru>
2022-03-08 20:31:52 +0300

    Добавлена сущность карты

---
+++ ExplodingKittens.API.Base/Classes/Card.cs
@@ -0,0 +1,18 @@
+﻿using ExplodingKittens.API.Base.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplodingKittens.API.Base.Classes
+{
+    internal class Card : ICard
+    {
+        public string Description {get; private set;}
+        public Card(string description)
+        {
+            Description = description;
+        }
+    }
+}
